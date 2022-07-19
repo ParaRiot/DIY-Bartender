c7f172a660d86dd588c40fdb8cf85a570f09ae1b
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-07-19 18:29:24 -0400

    updated functionality to javascript files to pull from apis and display results

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
+// Random Cocktail API
+citySearchBtn.onclick = async function getApi(event) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '6e6c108975msh9698f923c48000cp144f87jsnf6fc9adea9d2',
+            'X-RapidAPI-Host': 'cocktails3.p.rapidapi.com'
+        }
+    };
+    
+    let randomData = await getData('https://cocktails3.p.rapidapi.com/random', options);
+    console.log("this is random", randomData);
+    
+    // Activates display function for cocktail name and ingredients list
+    displayResultsList(randomData, randomData.body[0].name);
+}
 
+// Pulls cocktail random data 
+async function getData(URL, options) {
+    try {
+        let res = await fetch(URL, options);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
--- assets/script1.js
+++ assets/script1.js
@@ -1,48 +1,52 @@
+// Constant for youtube API Key
+const youtubeApiKey = 'AIzaSyDwqNTOnesmX68ISLWg0AwyCXPGZWwlpCY'
 
 // Function to display results
 async function displayResultsList(data, cocktailName) {
+    console.log("Made to method", data.body[0].ingredients);
 
+    let resultsContainerDiv = document.getElementById('result-box-Container');
     // Drink results to pull
-    var drinkResults = {
-
+    let newListUl = document.createElement('ol');
+    newListUl.id = 'cocktail-info-content';
+    for (var i = 0; i < data.body[0].ingredients.length; i++) {
+        let newListItem = document.createElement('li');
+        newListItem.textContent = data.body[0].ingredients[i];
+        newListItem.id = data.body[0].ingredients[i];
+        newListUl.appendChild(newListItem);
     }
 
-    // YouTube results to pull
-    var videoResults = {
-        video: contents[0].video,
-    }
+    let resultHeader = document.createElement('h2');
 
-    // Variables
-    let resultBoxContainer = document.createElement('div');
-    let resultDrinkHeader = document.createElement('h2');
-    let resultDrinkInfo = document.createElement('p');
-    let resultVideoLink = document.createElement('src');
-    resultVideoLink.alt = 'YouTube Drink Video';
-    // resultVideoLink.src = ;  ---- Need to pull src variable
+    // Makes header and appends to main results container div
+    resultHeader.id = "cocktail-name";
+    resultHeader.textContent = cocktailName;
+    resultsContainerDiv.appendChild(resultHeader);
+    resultsContainerDiv.appendChild(newListUl);
 
-    resultDrinkHeader.textContent = cocktailName + " Result";
-    // resultDrinkInfo.textContent = drinkResults;
+    const options = {
+        method: 'GET'
+    };    
     
-    // Appends children to main div
-    resultBoxContainer.appendChild(resultDrinkHeader);
-    resultBoxContainer.appendChild(resultDrinkInfo);
-    resultBoxContainer.appendChild(resultVideoLink);
+    
+    //Section to pull youtube API
+    var url = new URL("https://www.googleapis.com/youtube/v3/search");
 
-    //Adds new elements for results -- will update later
-
-    // Assigns IDs -- will update later
-
-    // Assigns inner text with data -- will update later
-
-    // Appends results children -- will update later
-
-}
-
-// Displays other 2 or 3 results
-async function displayAdditionalResults(data, cocktailName) {
-
-    // Results variable for 2nd result
-    var secondResult = {
-
+    var data = {
+        "key": youtubeApiKey,
+        "part": "snippet",
+        "q": "how to make " + cocktailName,
+        "maxResults": 1,
     }
+    // For loop for params, alters youtube link to match what random drink was pulled, then creates a how to video using the video id
+    for (let k in data) { url.searchParams.append(k, data[k]); }
+    console.log("This is URL", url.toString());
+    let youtubeData = await (await fetch(url)).json();
+    console.log("this is youtube", youtubeData);
+    let youtubeDataSrc = document.createElement('a');
+    youtubeDataSrc.textContent = 'https://www.youtube.com/watch?v=' + youtubeData.items[0].id.videoId;
+    youtubeDataSrc.href = 'https://www.youtube.com/watch?v=' + youtubeData.items[0].id.videoId;
+
+    // Appends youtube link to main results container div
+    resultsContainerDiv.appendChild(youtubeDataSrc);
 }
