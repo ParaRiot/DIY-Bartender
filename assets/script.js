7a0d28511c76534252f6e5757858a7d59b10a4ca
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-07-19 21:28:28 -0400

    Switched to search API, re-did functionality for new API, created three divs with results, needs cleanup but works.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,22 @@
-// Random Cocktail API
+// Based on ingredient input - Cocktail API
 citySearchBtn.onclick = async function getApi(event) {
+    var input = document.getElementById('ingredientInput').value;
+    // console.log("this is input", input)    
+
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '6e6c108975msh9698f923c48000cp144f87jsnf6fc9adea9d2',
-            'X-RapidAPI-Host': 'cocktails3.p.rapidapi.com'
+            'X-RapidAPI-Host': 'cocktail-by-api-ninjas.p.rapidapi.com'
         }
     };
     
-    let randomData = await getData('https://cocktails3.p.rapidapi.com/random', options);
-    console.log("this is random", randomData);
+    var cocktailURL = 'https://cocktail-by-api-ninjas.p.rapidapi.com/v1/cocktail?name=' + input;
+    let resultData = await getData(cocktailURL, options);
+    // console.log("this is data", resultData[0].name);
     
     // Activates display function for cocktail name and ingredients list
-    displayResultsList(randomData, randomData.body[0].name);
+    await displayResultsList(resultData);
 }
 
 // Pulls cocktail random data 
--- assets/script1.js
+++ assets/script1.js
@@ -1,52 +1,114 @@
 // Constant for youtube API Key
 const youtubeApiKey = 'AIzaSyDwqNTOnesmX68ISLWg0AwyCXPGZWwlpCY'
 
+
+
 // Function to display results
-async function displayResultsList(data, cocktailName) {
-    console.log("Made to method", data.body[0].ingredients);
+async function displayResultsList(resultData) {
+    // console.log("Made to method", resultData[0].ingredients);
 
     let resultsContainerDiv = document.getElementById('result-box-Container');
+
+    // let resultsTwoContainerDiv = document.getElementById('result-box-Container-two');
+    // let resultsThreeContainerDiv = document.getElementById('result-box-Container-three');
+
     // Drink results to pull
-    let newListUl = document.createElement('ol');
-    newListUl.id = 'cocktail-info-content';
-    for (var i = 0; i < data.body[0].ingredients.length; i++) {
-        let newListItem = document.createElement('li');
-        newListItem.textContent = data.body[0].ingredients[i];
-        newListItem.id = data.body[0].ingredients[i];
-        newListUl.appendChild(newListItem);
+
+
+    // let newListUlTwo = document.createElement('ol');
+    // newListOlTwo.id = 'cocktail-info-content-two';
+
+    // let newListUlThree = document.createElement('ol');
+    // newListOlThree.id = 'cocktail-info-content-three';
+
+    for (var i = 0; i < 3; i++){
+        let resultsOneContainerDiv = await cocktailResultOne(resultData[i]);
+        let cocktailName = resultData[i].name;
+        let youtubeDataSrc = await getYoutubeUrl(cocktailName);
+
+        // console.log("this is cocktailName", cocktailName);
+        resultsContainerDiv.appendChild(resultsOneContainerDiv);
+
+        resultsOneContainerDiv.appendChild(youtubeDataSrc);
     }
 
-    let resultHeader = document.createElement('h2');
+    // for (var i = 0; i < resultData[0]; i++) {
+    //     let newListItem = document.createElement('li');
+    //     newListItem.textContent = data.ingredients[i];
+    //     newListItem.id = data[0].ingredients[i];
+    //     newListUl.appendChild(newListItem);
+    // }
+
+    // let resultHeader = document.createElement('h2');
+
+    // // Makes header and appends to main results container div
+    // resultHeader.id = "cocktail-name";
+    // resultHeader.textContent = cocktailName;
+    // resultsOneContainerDiv.appendChild(resultHeader);
+    // resultsContainerDiv.appendChild(newListUl);
+
+}
+
+async function cocktailResultOne(resultData) {
+
+    let resultsOneContainerDiv = document.createElement('div');
+    resultsOneContainerDiv.id = "result-one-container";
+
+    let newListOlOne = document.createElement('ol');
+    newListOlOne.id = 'cocktail-info-content-one';
+
+    for (var i = 0; i < resultData.ingredients.length; i++) {
+        let newListItemOne = document.createElement('ul');
+        newListItemOne.textContent = resultData.ingredients[i];
+        console.log("This is ingredients", resultData.ingredients[i]);
+        newListItemOne.id = resultData.ingredients[i];
+        newListOlOne.appendChild(newListItemOne);
+    }
+    let resultOneHeader = document.createElement('h2');
 
     // Makes header and appends to main results container div
-    resultHeader.id = "cocktail-name";
-    resultHeader.textContent = cocktailName;
-    resultsContainerDiv.appendChild(resultHeader);
-    resultsContainerDiv.appendChild(newListUl);
+    resultOneHeader.id = "cocktail-name-one";
+    resultOneHeader.textContent = resultData.name;
+    resultsOneContainerDiv.appendChild(resultOneHeader);
+    resultsOneContainerDiv.appendChild(newListOlOne);
+    // console.log("This is results container", resultsOneContainerDiv);
+    return resultsOneContainerDiv;
+    
+}
 
+// async function cocktailResultTwo(resultData) {
+//     for (var i = 0; i < resultData[1]; i++) {
+//     let newListItemTwo = document.createElement('ul');
+//     newListItemTwo.textContent = data.ingredients[i];
+//     newListItemTwo.id = resultData[1].ingredients[i];
+//     newListOlOne.appendChild(newListItem);
+// }
+// }
+
+async function getYoutubeUrl(cocktailName){
+    // Start of YouTube API section
     const options = {
         method: 'GET'
     };    
-    
-    
-    //Section to pull youtube API
+
+    //Section to pull YouTube API
     var url = new URL("https://www.googleapis.com/youtube/v3/search");
 
     var data = {
         "key": youtubeApiKey,
         "part": "snippet",
-        "q": "how to make " + cocktailName,
+        "q": "how to make a " + cocktailName + ", cocktail",
         "maxResults": 1,
     }
     // For loop for params, alters youtube link to match what random drink was pulled, then creates a how to video using the video id
     for (let k in data) { url.searchParams.append(k, data[k]); }
-    console.log("This is URL", url.toString());
+    // console.log("This is URL", url.toString());
     let youtubeData = await (await fetch(url)).json();
-    console.log("this is youtube", youtubeData);
+    // console.log("this is youtube", youtubeData);
     let youtubeDataSrc = document.createElement('a');
     youtubeDataSrc.textContent = 'https://www.youtube.com/watch?v=' + youtubeData.items[0].id.videoId;
     youtubeDataSrc.href = 'https://www.youtube.com/watch?v=' + youtubeData.items[0].id.videoId;
 
+    return youtubeDataSrc;
     // Appends youtube link to main results container div
-    resultsContainerDiv.appendChild(youtubeDataSrc);
 }
