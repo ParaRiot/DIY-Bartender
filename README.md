bdcb193b669b5908fd6e84f2c4c18db26f32975f
Mikami Archer <archermikami@gmail.com>
2022-07-20 20:03:12 -0400

    mikami code

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # DIY-Bartender
 I enjoy JavaScript because it utilizes both functional programming and object-oriented programming.
-something diff
--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
+var apiEndpoint = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=dogs&maxResults=1 ";
 
+
+// responsible for making API call
+function getAPI() {
+  fetch(apiEndpoint)
+  .then(function(res) {
+    return res.json()
+  })
+  .then(function(data) {
+    // do something with that data
+    console.log(data)
+  })
+}
+
+getAPI()
